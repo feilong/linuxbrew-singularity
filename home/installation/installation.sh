775b43e74e1dac36b7a594f797b9595aeb6e438f
Feilong Ma <mafeilong@gmail.com>
2017-07-31 16:09:48 -0400

    Add script to install packages using linuxbrew and pip.

---
+++ home/installation/installation.sh
@@ -0,0 +1,41 @@
+git clone https://github.com/Linuxbrew/brew.git ~/.linuxbrew
+
+export LANG="en_US.utf8"
+
+export PATH="$HOME/.linuxbrew/bin:$PATH"
+export MANPATH="$(brew --prefix)/share/man:$MANPATH"
+export INFOPATH="$(brew --prefix)/share/info:$INFOPATH"
+
+brew install python
+pip2 install --upgrade pip setuptools
+
+brew install python3
+pip3 install --upgrade pip setuptools wheel
+
+brew install numpy --with-python3
+brew install scipy --with-python3
+
+pip2 install --upgrade nibabel ipython scikit-learn 
+pip3 install --upgrade nibabel ipython scikit-learn 
+
+brew install hdf5 --with-fortran
+pip2 install --upgrade h5py
+pip3 install --upgrade h5py
+
+brew install matplotlib --with-python3
+
+# # TODO
+# # shogun (use linuxbrew?)
+
+# # brew install wxpython
+# brew install vtk --with-python3
+
+# # matplotlib and vtk has more build options
+# # config Perl before installing vtk
+# # cpan -i XML::Parser
+# brew install perl
+# brew postinstall perl
+
+brew list --versions | tee brewed_packages.txt
+pip2 freeze | tee python2_packages.txt
+pip3 freeze | tee python3_packages.txt
