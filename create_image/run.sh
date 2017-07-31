7e60173692689f1e8782d4724c824c3e97185360
Ma, Feilong <mafeilong@gmail.com>
2017-07-31 15:12:16 -0400

    Add files that generate singularity image.

---
+++ create_image/run.sh
@@ -0,0 +1,8 @@
+# Definition file references
+# https://github.com/neurodebian/neurodebian/master/containers/singularity-neurodebian.def
+# https://github.com/singularityware/singularity/blob/master/examples/debian/Singularity
+
+img=id-studies.img ; sudo rm -f $img; sudo singularity create -s 1024 $img && sudo singularity bootstrap $img jessie.def
+
+# Reducing image size if needed
+# https://github.com/singularityware/singularity/issues/623
