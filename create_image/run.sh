44e8e29de781910755b9975af75330d037da3cac
Feilong Ma <mafeilong@gmail.com>
2017-08-03 18:43:17 -0400

    Fix reference URL.

--- create_image/run.sh
+++ create_image/run.sh
@@ -1,5 +1,5 @@
 # Definition file references
-# https://github.com/neurodebian/neurodebian/master/containers/singularity-neurodebian.def
+# https://github.com/neurodebian/neurodebian/blob/master/containers/singularity-neurodebian.def
 # https://github.com/singularityware/singularity/blob/master/examples/debian/Singularity
 
 img=id-studies.img ; sudo rm -f $img; sudo singularity create -s 1024 $img && sudo singularity bootstrap $img jessie.def
